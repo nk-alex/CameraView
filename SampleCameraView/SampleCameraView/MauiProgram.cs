83f80fb6610e107c56b532e54ac7c6c840ac4b98
Alex Ghenghiu <galex@nksuite.es>
2024-08-06 12:21:18 +0200

    sample

---
+++ SampleCameraView/SampleCameraView/MauiProgram.cs
@@ -0,0 +1,22 @@
+﻿using CommunityToolkit.Maui;
+
+namespace SampleCameraView;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UsePrismApp<App>(PrismStartup.Configure)
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            })
+            .UseMauiCommunityToolkitCamera()
+            .UseMauiCommunityToolkit();
+
+        return builder.Build();
+    }
+}
---
+++ SampleCameraView/SampleCameraView/Views/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SampleCameraView.Views;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
+
